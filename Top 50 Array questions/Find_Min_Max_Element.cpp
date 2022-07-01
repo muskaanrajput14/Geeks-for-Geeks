55f8ec7da43f3fbe211d71d90d07c1308d97c8f4
Muskaan <muskaanrajput14@gmail.com>
2022-07-01 20:45:51 +0530

    third commit

--- Top 50 Array questions/Find_Min_Max_Element.cpp
+++ Top 50 Array questions/Find_Min_Max_Element.cpp
@@ -1,6 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
+#define ll long long
+
+pair<long long, long long> getMinMax(long long a[], int n) ;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        ll a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        pair<ll, ll> pp = getMinMax(a, n);
+
+        cout << pp.first << " " << pp.second << endl;
+    }
     return 0;
+}// } Driver Code Ends
+
+/*
+pair<long long, long long> getMinMax(long long a[], int n) 
+{
+sort(a.begin(), a.end());
+cout<<
+}
+*/
+
+pair<long long, long long> getMinMax(long long arr[], int n) 
+{
+    pair <long long ,long long >p;
+   sort(arr,arr+n);
+   p.first=arr[0];
+   p.second=arr[n-1];
+   return p;
 }
