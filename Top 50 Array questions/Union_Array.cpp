a3488fcdb5a7d8b91603ae755e17c264308e19f0
Muskaan <muskaanrajput14@gmail.com>
2022-07-01 20:37:47 +0530

    third commit

---
+++ Top 50 Array questions/Union_Array.cpp
@@ -0,0 +1,54 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template in C++
+
+class Solution{
+    public:
+    //Function to return the count of number of elements in union of two arrays.
+    int doUnion(int a[], int n, int b[], int m)  
+    {
+        map<int,int>freq;
+        int i;
+        
+        for(i=0;i<n;i++){
+            freq[a[i]]++;
+        }
+        
+        for(i=0;i<m;i++){
+            freq[b[i]]++;
+        }
+        
+        return freq.size();
+        
+        
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin >> t;
+	
+	while(t--){
+	    
+	    int n, m;
+	    cin >> n >> m;
+	    int a[n], b[m];
+	   
+	    for(int i = 0;i<n;i++)
+	       cin >> a[i];
+	       
+	    for(int i = 0;i<m;i++)
+	       cin >> b[i];
+	    Solution ob;
+	    cout << ob.doUnion(a, n, b, m) << endl;
+	    
+	}
+	
+	return 0;
+}  // } Driver Code Ends
